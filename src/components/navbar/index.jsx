7c3f625e091350c9752b98b7e8c433b527f16062
alexvedt <alexvedt@gmail.com>
2023-10-06 16:05:30 +0200

    Started with dark mode

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,11 +4,16 @@ import { NAVIGATION } from "../../lib/constants";
 export default function Navigation() {
   return (
     <nav>
+      
       {NAVIGATION.map((item) => (
         <li key={item.href}>
           <Link to={item.href}>{item.label}</Link>
+
         </li>
       ))}
+
     </nav>
+
+    
   );
 }
