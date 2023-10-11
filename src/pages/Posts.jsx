dbb257c8c1c60685abdf590cb1a656cdfa882af6
alexvedt <alexvedt@gmail.com>
2023-10-11 13:32:17 +0200

    Hardcoded card ready to fetch

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,20 +1,89 @@
-/**
- * Displays a list of posts
- * @see https://docs.noroff.dev/social-endpoints/posts
- */
-
-
 export default function PostsPage() {
   return (
     <>
-<div className="hero min-h-screen bg-error">
-  <div className="hero-content text-center">
-    <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
-      <p className="py-6 neutral">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-warning">Get Started</button>
-    </div>
-  </div>
-</div>    </>
+      <div className="min-h-screen md:container md:mx-auto bg-base-100 flex flex-wrap justify-center">
+        <div className="w-full md:w-1/3 p-2">
+          <div className="card w-full h-[300px] md:h-auto md:w-4/5 glass">
+            <div className="flex flex-row py-4">
+              <div className="avatar">
+                <div className="w-16 rounded-full mx-5">
+                  <img className="object-cover w-full h-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" />
+                </div>
+              </div>
+              <p className="flex-end text-sm capitalize pr-2 py-4">By User1 10.10.23 12:22</p>
+            </div>
+
+            <figure className="max-h-[200px] overflow-hidden">
+              <img className="object-cover w-full h-full" src="https://source.unsplash.com/random" alt="!" />
+            </figure>
+
+            <div className="card-body prose">
+              <h2 className="card-title">How to make website?</h2>
+              <p className="text-start">Today I learned that making a website is like skiing with rollerskates..?</p>
+              <div className="card-actions justify-end">
+                <a className="link link-hover link-accent">Read more..</a>
+              </div>
+            </div>
+            <div className="card-actions justify-end">
+            </div>
+          </div>
+        </div>
+        {/* You can duplicate this div for each additional card */}
+        <div className="w-full md:w-1/3 p-2">
+          <div className="card w-full h-[300px] md:h-auto md:w-4/5 glass">
+            <div className="flex flex-row py-4">
+              <div className="avatar">
+                <div className="w-16 rounded-full mx-5">
+                  <img className="object-cover w-full h-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" />
+                </div>
+              </div>
+              <p className="flex-end text-sm capitalize pr-2 py-4">By User1 10.10.23 12:22</p>
+            </div>
+
+            <figure className="max-h-[200px] overflow-hidden">
+              <img className="object-cover w-full h-full" src="https://source.unsplash.com/random" alt="!" />
+            </figure>
+
+            <div className="card-body prose">
+              <h2 className="card-title">How to make website?</h2>
+              <p className="text-start">Today I learned that making a website is like skiing with rollerskates..?</p>
+              <div className="card-actions justify-end">
+                <a className="link link-hover link-accent">Read more..</a>
+              </div>
+            </div>
+            <div className="card-actions justify-end">
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full md:w-1/3 p-2">
+          <div className="card w-full h-[300px] md:h-auto md:w-4/5 glass">
+            <div className="flex flex-row py-4">
+              <div className="avatar">
+                <div className="w-16 rounded-full mx-5">
+                  <img className="object-cover w-full h-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" />
+                </div>
+              </div>
+              <p className="flex-end text-sm capitalize pr-2 py-4">By User1 10.10.23 12:22</p>
+            </div>
+
+            <figure className="max-h-[200px] overflow-hidden">
+              <img className="object-cover w-full h-full" src="https://source.unsplash.com/random" alt="!" />
+            </figure>
+
+            <div className="card-body prose">
+              <h2 className="card-title">How to make website?</h2>
+              <p className="text-start">Today I learned that making a website is like skiing with rollerskates..?</p>
+              <div className="card-actions justify-end">
+                <a className="link link-hover link-accent">Read more..</a>
+              </div>
+            </div>
+            <div className="card-actions justify-end">
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    </>
   );
 }
