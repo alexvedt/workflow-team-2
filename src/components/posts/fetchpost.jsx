ab2c6e6cc1a1bcd32d6fbaa90ac73e56df6bf303
Ada Keita <mathildebrakstad@gmail.com>
2023-10-12 12:51:16 +0200

    feat: :construction: updated and refatctored som of the fetch logic
    Added an "add post" form need styling and functionality is limted until id assignment is fixed

--- src/components/posts/fetchpost.jsx
+++ src/components/posts/fetchpost.jsx
@@ -1,5 +1,6 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { apiKey, baseURL } from "../../lib/api";
+import { getNewestNonEmptyPosts, sortPostsDescending, limitPosts } from '../fetch-utils';
 
 export const useFetchPosts = () => {
     const [posts, setPosts] = useState([]);
@@ -24,13 +25,13 @@ export const useFetchPosts = () => {
             const data = await response.json();
 
             // Sort posts by created date
-            const sortedPosts = data.sort((a, b) => new Date(b.created) - new Date(a.created));
+            const sortedPosts = sortPostsDescending(data);
 
-            // Filter out posts with empty body
-            const nonEmptyPosts = sortedPosts.filter(post => post.body.trim() !== '');
+            // Filter out empty posts
+            const nonEmptyPosts = getNewestNonEmptyPosts(sortedPosts);
 
             // Limit to 9
-            const limitedPosts = nonEmptyPosts.slice(0, 9);
+            const limitedPosts = limitPosts(nonEmptyPosts, 9);
 
             setPosts(limitedPosts);
         } catch (error) {
@@ -41,4 +42,52 @@ export const useFetchPosts = () => {
     }, []);
 
     return { posts, isLoading, error, fetchData };
+};
+
+export const useFetchCurrentUserPosts = (userId) => {
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const fetchCurrentUserPosts = useCallback(async () => {
+        try {
+            setIsLoading(true);
+            const accessToken = apiKey;
+            const response = await fetch(`${baseURL}/social/posts?_author=${userId}`, {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            // Sort posts by created
+            const sortedPosts = sortPostsDescending(data);
+
+            // Filter out empty posts
+            const nonEmptyPosts = getNewestNonEmptyPosts(sortedPosts);
+
+            // Limit to 9
+            const limitedPosts = limitPosts(nonEmptyPosts, 9);
+
+            setPosts(limitedPosts);
+        } catch (error) {
+            setError(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }, [userId]); // Include userId for profile page
+
+    useEffect(() => {
+        if (userId) {
+            fetchCurrentUserPosts();
+        }
+    }, [userId, fetchCurrentUserPosts]);
+
+    return { posts, isLoading, error };
 };
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,36 @@
-/**
- * Displays a single profile
- * @see https://docs.noroff.dev/social-endpoints/profiles
- */
+import { useState } from "react";
+import { useFetchCurrentUserPosts } from "../components/posts/fetchpost";
+import { PostForm } from "../components/posts/addpost";
+
 export default function ProfilePage() {
+  const { posts, isLoading, error } = useFetchCurrentUserPosts();
+  const [updatedPosts, setUpdatedPosts] = useState(posts);
+
+  const handleAddPost = (newPost) => {
+    const updatedPosts = [newPost, ...posts];
+    setUpdatedPosts(updatedPosts);
+  };
+
+  if (isLoading) return <h1>Loading...</h1>;
+
+  if (error) {
+    return (
+      <div className="bg-red-500">
+        <h1 className="">Something went wrong! {error?.message}</h1>
+      </div>
+    );
+  }
+
   return (
     <>
       <h1>Profile Page</h1>
+      <PostForm onAddPost={handleAddPost} />
+
+      <section>
+        {posts.map((post) => (
+          <div key={post.id}>{post?.title}</div>
+        ))}
+      </section>
     </>
   );
 }
