12fb899bd2e4d3ddbf28f7e131633729a4c2a242
Ada Keita <mathildebrakstad@gmail.com>
2023-10-12 19:49:48 +0200

    working on postbtn

---
+++ src/components/postbtn/index.jsx
@@ -0,0 +1,13 @@
+export default function PostBtn() {
+    return (
+        <div className="postbtn">
+            <div className="postbtn__container">
+                <div className="postbtn__container--btn">
+                    <button className="btn btn-primary btn-circle btn-l">
+
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
