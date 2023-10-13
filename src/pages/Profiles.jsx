c3578c065ff49c4c35fe518398fc6e12cd6cf0cb
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-10-13 15:40:54 +0200

    start profiles

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -2,9 +2,12 @@
  * Displays a list of profiles
  * @see https://docs.noroff.dev/social-endpoints/profiles
  */
+import Navigation from "../components/navbar";
+
 export default function ProfilesPage() {
   return (
     <>
+      <Navigation />
       <h1>Many Profiles Page</h1>
     </>
   );
