ef1a3b8a1901197f1adda130def372ecfbdce7be
alexvedt <alexvedt@gmail.com>
2023-10-13 14:34:22 +0200

    fixed page issue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,10 +36,14 @@ export default function PostsPage() {
       <header>
         <Navigation />
       </header>
+
+      <section id="hero-home">
+        
+      </section>
       <section id="feed " className="w-full bg-base-100">
       <div className="min-h-screen md:container md:mx-auto  flex sm:flex-col flex-wrap content-center justify-center overflow-x-hidden ">
         {posts.map((post) => (
-          <div key={post.id} className="w-full md:w-2/3 p-2 mb-2.5 sm:w-10/12 flex content-center">
+          <div key={post.id} className="w-full md:w-2/3 p-2 mb-2.5 sm:w-10/12 flex content-center justify-center">
             <div className="card w-full max-w-[80%] h-[300px] md:h-auto md:w-4/5 glass">
               <div className="flex flex-row py-4">
                 <div className="avatar">
@@ -54,13 +58,13 @@ export default function PostsPage() {
               </figure>
               <div className="card-body prose">
                 <h2 className="card-title truncate max-w-full h-[40px]">
-                  <a href={`/posts/${post.id}`}>{post?.title}</a>
+                  <a href={`/post/?id=${post.id}`}>{post?.title}</a>
                 </h2>
                 <p className="text-start h-[60px] overflow-hidden">
                   {post?.body.length > 50 ? post.body.substr(0, 50) + "..." : post.body}
                 </p>
                 <div className="card-actions justify-between">
-                  <a className="link link-hover link-accent" href={`/posts/${post.id}`}>Read more</a>
+                  <a className="link link-hover link-accent" href={`/post/?id=${post.id}`}>Read more</a>
                   <button onClick={() => handleLike(post.id)} className={`focus:outline-none ${pulse[post.id] ? 'animate-pulse' : ''}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill={likedPosts[post.id] ? "red" : "none"} viewBox="0 0 24 24" stroke="currentColor">
                       <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
