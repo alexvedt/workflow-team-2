0b2b1103a19e0d275a1203bd236968590c0c2144
Ada Keita <mathildebrakstad@gmail.com>
2023-10-14 15:27:09 +0200

    btn-design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,6 +41,8 @@ export default function PostsPage() {
 
       <section id="hero-home">
 
+        <PostBtn />
+
       </section>
       <section id="feed " className="w-full bg-base-100">
         <div className="min-h-screen md:container md:mx-auto  flex sm:flex-col flex-wrap content-center justify-center overflow-x-hidden ">
