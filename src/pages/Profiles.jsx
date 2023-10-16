dde6fc140e76954479e1f8888b2325b9bc0750bb
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-10-16 17:16:23 +0200

    profiles list

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -3,12 +3,18 @@
  * @see https://docs.noroff.dev/social-endpoints/profiles
  */
 import Navigation from "../components/navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ExampleProfiles from "../components/profiles";
 
 export default function ProfilesPage() {
   return (
     <>
       <Navigation />
-      <h1>Many Profiles Page</h1>
+      <Router>
+        <Routes>
+          <Route path="/profiles" element={<ExampleProfiles />} />
+        </Routes>
+      </Router>
     </>
   );
 }
