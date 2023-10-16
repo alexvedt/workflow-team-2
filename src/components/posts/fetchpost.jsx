2f927983178f60bf5217f94eb80b2448124c7ebe
alexvedt <alexvedt@gmail.com>
2023-10-16 21:43:25 +0200

    Fix:profilefetch

--- src/components/posts/fetchpost.jsx
+++ src/components/posts/fetchpost.jsx
@@ -1,96 +1,102 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from "react";
 import { apiKey, baseURL } from "../../lib/api";
-import { getNewestNonEmptyPosts, sortPostsDescending, limitPosts } from '../fetch-utils';
+import {
+  getNewestNonEmptyPosts,
+  sortPostsDescending,
+  limitPosts,
+} from "../fetch-utils";
 
 export const useFetchPosts = () => {
-    const [posts, setPosts] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const fetchData = useCallback(async () => {
-        try {
-            setIsLoading(true);
-            const accessToken = apiKey;
-            const response = await fetch(`${baseURL}/social/posts`, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${accessToken}`
-                }
-            });
+  const fetchData = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const accessToken = apiKey;
+      const response = await fetch(`${baseURL}/social/posts`, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-            const data = await response.json();
-            console.log(data, 'data');
+      const data = await response.json();
+      console.log(data, "data");
 
-            // Sort posts by created date
-            const sortedPosts = sortPostsDescending(data);
+      // Sort posts by created date
+      const sortedPosts = sortPostsDescending(data);
 
-            // Filter out empty posts
-            const nonEmptyPosts = getNewestNonEmptyPosts(sortedPosts);
+      // Filter out empty posts
+      const nonEmptyPosts = getNewestNonEmptyPosts(sortedPosts);
 
+      // Limit to 9
+      const limitedPosts = limitPosts(nonEmptyPosts, 9);
 
-            // Limit to 9
-            const limitedPosts = limitPosts(nonEmptyPosts, 9);
+      setPosts(limitedPosts);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
 
-            setPosts(limitedPosts);
-        } catch (error) {
-            setError(error);
-        } finally {
-            setIsLoading(false);
-        }
-    }, []);
-
-    return { posts, isLoading, error, fetchData };
+  return { posts, isLoading, error, fetchData };
 };
 
 export const useFetchCurrentUserPosts = (username, jwt) => {
-    const [posts, setPosts] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    const fetchCurrentUserPosts = useCallback(async () => {
-        try {
-            setIsLoading(true);
-            const response = await fetch(`${baseURL}/social/profiles/${username}/posts`, {
-                method: "GET",
-                headers: {
-                    Authorization: `Bearer ${jwt}`,
-                },
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const data = await response.json();
-
-            // Sort posts by created
-            const sortedPosts = sortPostsDescending(data);
-
-            // Filter out empty posts
-            const nonEmptyPosts = getNewestNonEmptyPosts(sortedPosts);
-
-            // Limit to 9
-            const limitedPosts = limitPosts(nonEmptyPosts, 9);
-
-            setPosts(limitedPosts);
-        } catch (error) {
-            setError(error);
-        } finally {
-            setIsLoading(false);
+  const fetchCurrentUserPosts = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(
+        `${baseURL}/social/profiles/${username}/posts`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${jwt}`,
+          },
         }
-    }, [username, jwt]);
+      );
 
-    console.log(username);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
 
-    useEffect(() => {
-        if (username && jwt) {
-            fetchCurrentUserPosts();
-        }
-    }, [username, jwt, fetchCurrentUserPosts]);
+      const data = await response.json();
 
-    return { posts, isLoading, error, username };
+      // Sort posts by created
+      const sortedPosts = sortPostsDescending(data);
+
+      // Filter out empty posts
+      const nonEmptyPosts = getNewestNonEmptyPosts(sortedPosts);
+
+      // Limit to 9
+      const limitedPosts = limitPosts(nonEmptyPosts, 9);
+
+      setPosts(limitedPosts);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [username, jwt]);
+
+  console.log(username);
+
+  useEffect(() => {
+    if (username && jwt) {
+      fetchCurrentUserPosts();
+    }
+  }, [username, jwt, fetchCurrentUserPosts]);
+
+  return { posts, isLoading, error, username };
 };
