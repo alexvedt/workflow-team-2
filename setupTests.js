ecedb180929d00f333f767dd86497a5426e90f2b
alexvedt <alexvedt@gmail.com>
2023-11-07 18:15:54 +0100

    not.fix

---
+++ setupTests.js
@@ -0,0 +1,14 @@
+// setupTests.js
+import "@testing-library/jest-dom";
+
+
+"jest": {
+    "testEnvironment": "jsdom",
+    "moduleNameMapper": {
+      "^.+\\.svg$": "jest-svg-transformer",
+      "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+    },
+    "setupFilesAfterEnv": [
+      "<rootDir>/setupTests.js"
+    ]
+  }
